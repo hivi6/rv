5256501f4e69c9d15f39f35ad199aaf986608711
hivi6 <89378957+hivi6@users.noreply.github.com>
2026-09-20 15:48:58 +0530

    feat: Add support for BType instruction decoding

--- include/instruction.hpp
+++ include/instruction.hpp
@@ -48,6 +48,23 @@ struct SType {
 };
 
 template<RegisterType T>
+struct BType {
+	static constexpr u32 opcode(u32 i) { return i & 0x7f;         }
+	static constexpr u32 funct3(u32 i) { return (i >> 12) & 0x07; }
+	static constexpr u32 rs1(u32 i)    { return (i >> 15) & 0x1f; }
+	static constexpr u32 rs2(u32 i)    { return (i >> 20) & 0x1f; }
+	static constexpr T imm(u32 i) {
+		const auto imm_11   = (i >> 7) & 0b1;
+		const auto imm_4_1  = (i >> 8) & 0b1111;
+		const auto imm_10_5 = (i >> 25) & 0b111111;
+		const auto imm_12   = (i >> 31) & 0b1;
+		const auto res = (imm_12 << 12) | (imm_11 << 11) 
+			| (imm_10_5 << 5) | (imm_4_1 << 1);
+		return signExtend<T>(res, 13);
+	}
+};
+
+template<RegisterType T>
 struct UType {
 	static constexpr u32 opcode(u32 i)  { return i & 0x7f;         }
 	static constexpr u32 rd(u32 i)      { return (i >> 7) & 0x1f;  }
